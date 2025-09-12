b98738f4460037014f0c291f1582e7a7dff84652
timothijoe <zt1301112@gmail.com>
2025-09-12 13:16:45 +0800

    ok

--- taec_traj_designer/m4_visual_mpc_control.py
+++ taec_traj_designer/m4_visual_mpc_control.py
@@ -9,9 +9,9 @@ from taec_utils.mpc_utils import iterative_linear_mpc_control, calc_nearest_inde
 import math
 TRAJ_LIBRARY_PATH = DATA_RAW_FOLDER + '/traj/'
 MPC_LIBRARY_PATH =  DATA_RAW_FOLDER + '/mpc_track_traj/'
-MPC_VISUAL_LIBRARY_PATH =  DATA_RAW_FOLDER + '/mpc_visual/'
+MPC_VISUAL_LIBRARY_PATH =  DATA_RAW_FOLDER + '/mpc_visual2/'
 ensure_directory_exists(MPC_VISUAL_LIBRARY_PATH)
-
+import copy
 
 show_both_traj = False
 # show_both_traj = True
@@ -37,6 +37,29 @@ def load_pickle_files(folder_path):
     return trajectories_dict
 
 
+def plant_model_batch(prev_state_batch, pedal_batch, steering_batch, dt = 0.1, st_rate_constrain=0.5):
+    #import copy
+    prev_state = prev_state_batch
+    x_t = prev_state[0]
+    y_t = prev_state[1]
+    psi_t = prev_state[2]
+    v_t = prev_state[3]
+
+    beta = steering_batch
+    a_t = pedal_batch
+    v_t_1 = v_t + a_t * dt 
+    psi_dot = v_t * np.tan(beta) / 2.5
+    psi_t_1 = psi_dot*dt + psi_t 
+    x_dot = v_t_1 * np.cos(psi_t_1)
+    y_dot = v_t_1 * np.sin(psi_t_1)
+    x_t_1 = x_dot * dt + x_t 
+    y_t_1 = y_dot * dt + y_t
+    
+    #psi_t = self.wrap_angle_rad(psi_t)
+    current_state = np.array([x_t_1, y_t_1, psi_t_1, v_t_1])
+    #current_state = torch.FloatTensor([x_t, y_t, psi_t, v_t_1])
+    return current_state
+
 def track_trajectories(trajectories, output_folder, filename):
     """
     绘制一个文件中的多个轨迹，并保存为单独的图像。
@@ -55,18 +78,36 @@ def track_trajectories(trajectories, output_folder, filename):
         trajectory = track_trajectory
         x = trajectory[:, 0]  # x 坐标
         y = trajectory[:, 1]  # y 坐标
-        if (not show_both_traj):
-            plt.plot(x, y, label=f"Trajectory {i+1}")
-            norm = plt.Normalize(trajectory[:,0].min(), trajectory[:,0].max())
-            norm_lon = norm(trajectory[:,0])
-            # print(trajectory[:,:4])
-            #plt.scatter(trajectory[:,0], trajectory[:,1], c=norm_lon, cmap='viridis')
-            plt.scatter(trajectory[-20:,0], trajectory[-20:,1], cmap='viridis')
-        else:
-            ox = raw_trajectory[:, 0]
-            oy = raw_trajectory[:, 1]
-            plt.plot(ox, oy, color='red', linewidth = 2, label=f"Trajectory {i+1}")
-            plt.plot(x, y, color='green', linewidth = 2, label=f"Trajectory {i+1}")
+        init_state = trajectory[0][:4]
+        last_state = copy.deepcopy(init_state)
+        state_list = []
+        state_list.append(init_state)
+        control_list = trajectory_ele['track_action']
+        for i in range(len(control_list)):
+            if i == 0:
+                continue  
+            control = control_list[i]
+            my_state = plant_model_batch(last_state, 0, control[1])
+            state_list.append(my_state)
+            last_state = copy.deepcopy(my_state)
+        state_array = np.array(state_list)
+        sx = state_array[:, 0]  # x 坐标
+        sy = state_array[:, 1]  # y 坐标
+        plt.plot(x, y, color='red', linewidth = 3, label=f"Trajectory {i+1}")
+        plt.plot(sx, sy, color='green', linewidth = 2, label=f"Trajectory {i+1}")      
+            
+        # if (not show_both_traj):
+        #     plt.plot(x, y, label=f"Trajectory {i+1}")
+        #     norm = plt.Normalize(trajectory[:,0].min(), trajectory[:,0].max())
+        #     norm_lon = norm(trajectory[:,0])
+        #     # print(trajectory[:,:4])
+        #     #plt.scatter(trajectory[:,0], trajectory[:,1], c=norm_lon, cmap='viridis')
+        #     plt.scatter(trajectory[-20:,0], trajectory[-20:,1], cmap='viridis')
+        # else:
+        #     ox = raw_trajectory[:, 0]
+        #     oy = raw_trajectory[:, 1]
+        #     plt.plot(ox, oy, color='red', linewidth = 2, label=f"Trajectory {i+1}")
+        #     plt.plot(x, y, color='green', linewidth = 2, label=f"Trajectory {i+1}")
    
     plt.grid()
     plt.axis("equal")  # 保持 x 和 y 轴比例一致     
